84c1e0120713ec46916e169db56bcc5bd3f9e0f5
Gh0stbasta <ubuntuvj@gmail.com>
2026-01-03 20:15:57 +0000

    fixed localhost connection to container

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -1,7 +1,11 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
 
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-})
+  server: {
+    host: true, //necessary to get hold of the container workspace
+    port: 5173,
+  },
+});
